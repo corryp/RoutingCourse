92e8e3c41af2108c2835468ba7f1fba0395561e0
Paul Corry <p.corry@qut.edu.au>
2026-01-15 10:19:15 +1000

    cpu time for LS

--- prac1.cpp
+++ prac1.cpp
@@ -1,34 +1,37 @@
 #include "BasicTSPheuristics.hpp"
 #include "get_tsplib.hpp"
 
-void test_tsplib(string tsp_name) {
+void solve_tsplib(string tsp_name) {
 	vector<CityPair> v_dat;
 	vector<vector<double>> d_dist, d_xy;
-    vector<int> i_rnd_tour, i_nn_tour;
     
     int n = get_tsplib(tsp_name, v_dat, d_dist, d_xy);
-    i_rnd_tour.resize(n);
+    vector<int> i_rnd_tour(n);
     double d_zrnd = gd_random_tour(n, d_dist, i_rnd_tour);
 
-    i_nn_tour.resize(n);
+    vector<int> i_nn_tour(n);
     double d_znn = gd_nearest_neighbour(n, d_dist, 0, i_nn_tour);
 
     vector<int> i_2o_rnd_tour(i_rnd_tour);
     vector<int> i_2o_nn_tour(i_nn_tour);
 
+    clock_t t_start_rnd = clock();
     double d_z2o_rnd = gd_local_2opt_search(n, d_dist, i_2o_rnd_tour, true);
+    double d_cpu_rnd = (clock() - t_start_rnd) / (double)CLOCKS_PER_SEC;
+
+    clock_t t_start_nn = clock();
     double d_z2o_nn = gd_local_2opt_search(n, d_dist, i_2o_nn_tour, true);
+    double d_cpu_nn = (clock() - t_start_nn) / (double)CLOCKS_PER_SEC;
 
     cout << "z_rnd = " << d_zrnd << endl;
     cout << "z_nn = " << d_znn << endl;
-    cout << "z_2o_rnd = " << d_z2o_rnd << endl;
-    cout << "z_2o_rnd = " << d_z2o_nn << endl;
+    cout << "z_2o_rnd = " << d_z2o_rnd << " (CPU: " << d_cpu_rnd << "s)" << endl;
+    cout << "z_2o_nn = " << d_z2o_nn << " (CPU: " << d_cpu_nn << "s)" << endl;
 
     g_output_tour_csv(i_2o_nn_tour, d_xy);
 }
 
 int main() {
-    //g_test_heuristics();
-    test_tsplib("ch130");
+    solve_tsplib("ch130");
     return 0;
 }
